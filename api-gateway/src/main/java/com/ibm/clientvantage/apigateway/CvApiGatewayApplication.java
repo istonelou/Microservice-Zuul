0238dd4a524774e4031d9cf59750f25e10571bc9
Xiao Bo Li <lixiaobo@cn.ibm.com>
2017-12-03 07:45:08 +0800

    extend

--- api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
@@ -20,7 +20,7 @@ public class CvApiGatewayApplication {
 		SpringApplication.run(CvApiGatewayApplication.class, args);
 		
 		Timer timer = new Timer(); 
-        timer.schedule(new RetrieveClusterServices(), 15000, 5000);//start the task after 5 seconds, and execute every 5 seconds
+        timer.schedule(new RetrieveClusterServices(), 30000, 5000);//start the task after 5 seconds, and execute every 5 seconds
 	}
 	
 	static class RetrieveClusterServices extends java.util.TimerTask {
