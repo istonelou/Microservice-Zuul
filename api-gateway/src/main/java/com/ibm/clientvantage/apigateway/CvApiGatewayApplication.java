3b7661b9cf154d0583fc5231cda38d015967b3ac
Xiao Bo Li <lixiaobo@cn.ibm.com>
2017-12-03 07:47:31 +0800

    add log

--- api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
@@ -1,5 +1,7 @@
 package com.ibm.clientvantage.apigateway;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +17,20 @@ public class CvApiGatewayApplication {
     private static RefreshRouteService refreshRouteService;
     
 	private static boolean mClusterActive = true;
+	private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CvApiGatewayApplication.class, args);
 		
 		Timer timer = new Timer(); 
+		System.out.println(df.format(new Date()));
         timer.schedule(new RetrieveClusterServices(), 30000, 5000);//start the task after 5 seconds, and execute every 5 seconds
+        
 	}
 	
 	static class RetrieveClusterServices extends java.util.TimerTask {
-        public void run(){     
+        public void run(){
+        		System.out.println(df.format(new Date()));
         		refreshRouteService.refreshRoute();
         }     
     }   
