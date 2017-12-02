494391bfcb8c36d725703bb14da6910294bdabf9
Xiao Bo Li <lixiaobo@cn.ibm.com>
2017-12-02 10:47:09 +0800

    add mock mvc

--- api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
@@ -3,6 +3,11 @@ package com.ibm.clientvantage.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
 import com.ibm.clientvantage.apigateway.DemoController;
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.Configuration;
@@ -11,6 +16,9 @@ import io.kubernetes.client.models.V1Pod;
 import io.kubernetes.client.models.V1PodList;
 import io.kubernetes.client.util.Config;
 import io.kubernetes.client.ApiException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,12 +27,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CvApiGatewayApplication {
 	
 	@Autowired
-	private static DemoController controller;
+	private static MockMvc mvc;
 
-	public static void main(String[] args) throws IOException, ApiException {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CvApiGatewayApplication.class, args);
 		
-		controller.refreshRoute();
+		MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/refreshRoute"))
+				.andExpect(handler().handlerType(DemoController.class))
+	            .andReturn();  
 		
 		ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
