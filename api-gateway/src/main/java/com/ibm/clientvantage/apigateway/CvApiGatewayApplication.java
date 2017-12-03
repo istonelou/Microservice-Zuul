6d6a3d6918da1d6218207d17dc4f01c2cc3e162b
Xiao Bo Li <lixiaobo@cn.ibm.com>
2017-12-03 14:56:47 +0800

    commit

--- api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.ibm.clientvantage.apigateway;
 import java.util.Timer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -10,17 +11,19 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ibm.clientvantage.apigateway"})
-public class CvApiGatewayApplication {
+public class CvApiGatewayApplication implements CommandLineRunner{
 	
 	private static boolean mClusterActive = true;
-
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CvApiGatewayApplication.class, args);
-		
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
 		Timer timer = new Timer(); 
         timer.schedule(new RetrieveClusterServices(), 10000, 5000);//start the task after 5 seconds, and execute every 5 seconds
-	}  
+	}
 }
 
 class RetrieveClusterServices extends java.util.TimerTask{
