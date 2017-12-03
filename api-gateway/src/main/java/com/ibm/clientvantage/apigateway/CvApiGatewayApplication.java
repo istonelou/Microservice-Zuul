87a5d0c75b2549cef61ba6ceba37f487f378d23c
Xiao Bo Li <lixiaobo@cn.ibm.com>
2017-12-03 08:29:23 +0800

    change to use controller

--- api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
@@ -1,42 +1,33 @@
 package com.ibm.clientvantage.apigateway;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Timer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.ibm.clientvantage.apigateway.DemoController;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class CvApiGatewayApplication {
 	
 	private static boolean mClusterActive = true;
-	private static MockMvc mvc;
+	
+	@Autowired
+	private static DemoController controller;
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CvApiGatewayApplication.class, args);
 		
 		Timer timer = new Timer(); 
-        timer.schedule(new RetrieveClusterServices(), 5000, 5000);//start the task after 5 seconds, and execute every 5 seconds
+        timer.schedule(new RetrieveClusterServices(), 1000, 5000);//start the task after 5 seconds, and execute every 5 seconds
         
 	}
 	
 	static class RetrieveClusterServices extends java.util.TimerTask {
         public void run(){
-        		try {
-					mvc.perform(MockMvcRequestBuilders.get("/refreshRoute"))
-							.andDo(print());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+        		controller.refreshRoute();
         }     
     }   
 }
