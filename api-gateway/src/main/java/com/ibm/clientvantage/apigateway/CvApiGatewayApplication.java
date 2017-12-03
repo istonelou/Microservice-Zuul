d254186031cb7e8a553e3df8c73d74f0eb163437
Xiao Bo Li <lixiaobo@cn.ibm.com>
2017-12-03 08:50:13 +0800

    create the data

--- api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ibm/clientvantage/apigateway/CvApiGatewayApplication.java
@@ -6,28 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import com.ibm.clientvantage.apigateway.DemoController;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class CvApiGatewayApplication {
 	
 	private static boolean mClusterActive = true;
-	
-	@Autowired
-	private static DemoController controller;
-	
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CvApiGatewayApplication.class, args);
 		
 		Timer timer = new Timer(); 
         timer.schedule(new RetrieveClusterServices(), 1000, 5000);//start the task after 5 seconds, and execute every 5 seconds
-        
-	}
-	
-	static class RetrieveClusterServices extends java.util.TimerTask {
-        public void run(){
-        		controller.refreshRoute();
-        }     
-    }   
+	}  
 }
+
+class RetrieveClusterServices extends java.util.TimerTask{
+	
+	@Autowired
+	private RefreshRouteService mRefreshService;
+	
+	@Override  
+	public void run() {
+		mRefreshService.refreshRoute();
+		System.out.println("now start to retrieve the services");
+	}  
+}  
